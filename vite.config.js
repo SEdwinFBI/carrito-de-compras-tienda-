5de111a3e321b68ea32cb7e02977539d46fdfe6a
Edwin Baquaix <baquiaxfernando0@gmail.com>
2024-06-05 18:09:55 -0600

    prueba

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://SEdwinFBI.github.io/carrito-de-compras-tienda-',
+  base:'https://SEdwinFBI.github.io/carrito-de-compras-tienda-/',
   //server:{
     //host:'192.168.0.103'
   //}
